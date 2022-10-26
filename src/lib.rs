c551bf4d1f72cf0463c44506fb55a84687ccf051
Vinny Meller <vinnymeller@gmail.com>
2022-10-26 01:31:01 -0500

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,10 @@ async fn health_check() -> impl Responder {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
         .listen(listener)?
         .run();
     Ok(server)
