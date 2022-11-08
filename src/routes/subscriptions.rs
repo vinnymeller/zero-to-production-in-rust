a3e17feb75274dfb1971f2a16208850cf5646bb0
Vinny Meller <vinnymeller@gmail.com>
2022-11-07 23:03:12 -0600

    make the log messages correlatable

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,13 @@ pub async fn subscribe(
     form: web::Form<SubscriberFormData>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.name,
+        form.email
+    );
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES
@@ -24,13 +31,17 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection.get_ref())
-    .await {
+    .await
+    {
         Ok(_) => {
-            log::info!("New subscriber details have been saved.");
+            log::info!(
+                "request_id {} - New subscriber details have been saved.",
+                request_id
+            );
             HttpResponse::Ok().finish()
-        },
+        }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
