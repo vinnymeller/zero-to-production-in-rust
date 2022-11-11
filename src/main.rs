03e8c6b8f62f5e6f0bebf2b7e54ccd38a0bee300
Vinny Meller <vinnymeller@gmail.com>
2022-11-11 02:17:17 -0600

    currently runs BEAUTIFULLY

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -14,7 +14,10 @@ async fn main() -> std::io::Result<()> {
     let connection_string = configuration.database.connection_string();
     let pool = PgPool::connect_lazy(&connection_string.expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, pool)?.await
 }
